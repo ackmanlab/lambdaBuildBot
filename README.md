63336d1b6a200a731e3ac15b4547eea8eb40e975
ackman678 <james.ackman@gmail.com>
2017-09-15 13:40:53 -0700

    Flexible env variables
    * environment variable interpretation for local destination of site build and aws region. Allows for node-lambda to be easily used local testing

---
+++ README.md
@@ -0,0 +1,48 @@
+---
+date: 2017-09-15 13:05:40  
+title: lambdaBuildBot
+author: James Ackman  
+---
+
+lambdaBuildBot– A serverless build and deploy bot for site content.
+
+The idea is that upon commiting content changes to a project, the remote push/synchronization with a master branch of its github repo triggers this bot to build a project and deploy it for you. 
+
+Currently it is configured to use AWS Lambda and deploy to an S3 bucket, but since the main code is all standard nodejs javascript, one could presumably configure a version compatible with other cloud services (Microsoft Azure, Google, IBM Openwhisk, etc). Installation pulls in woola to use as the javascript markdown/html build tool. 
+
+
+## Installation
+
+Have node/npm installed on your local machine then:  
+
+    npm install https://github.com/ackmanlab/lambdaBuildBot.git
+
+
+## Deploy
+
+For AWS:
+
+1. Get an AWS account setup and S3 bucket configured (see Amazon documentation).
+2. Get a github access token.
+3. Create a lambda function with the name lambdaBuildBot. 
+  * Set the function up with at least 1024 MB memory so it will run fastest (cpu speed is tied to memory request requirement for lambda).
+4. Update function... if you have make a file called 'deploy.sh' with the following contents in your lambdaBuildBot directory and you have aws-cli installed then you can just run `npm deploy`:  
+
+
+```bash
+#!/bin/bash
+awsRegion='' #'us-west-2'
+awsAccountNumber='' #'xxxxxxxxxxxx'
+filepath=$(pwd)
+fname='lambdaBuildBot'
+zip -r $fname.zip index.js node_modules
+aws lambda update-function-code \
+--function-name arn:aws:lambda:$awsRegion:$awsAccountNumber:function:$fname \
+--zip-file fileb://$filepath/$fname.zip
+
+```
+
+With this workflow you can go from a project update in a git repository on a local computer to a completely rebuilt live static site accessible anywhere in the world within 2-4 s (with a cold invocation, repeated hot invocations would be even faster) giving rise to the possibility for fast asynchronous collaborative updates of complex apps.
+
+
+MIT License
--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const gunzip = zlib.createGunzip()
 
 const AWS = require('aws-sdk')
 AWS.config.region = process.env.AWS_REGION
-// console.log(AWS.config.credentials.accessKeyId)
-// console.log(AWS.config.region)
 const s3 = new AWS.S3()
 
 const mdbuild = require('woola')
 const appconfig = require('./node_modules/woola/lib/appconfig.js')
 
 const ghtoken = process.env.GITHUB_ACCESS_TOKEN
-const localStore = path.resolve('./tmp')
+const localStore = process.env.LOCAL_DIR
+// const localStore = path.resolve('./tmp')
 // const globPattern = '**/*'
 
 let site
@@ -48,7 +47,7 @@ const uploadStream = (file,s3bucket,srcPath='.') => {
     Body: fileStream,
     Key: file, 
     ContentType: computeContentType(file),
-    CacheControl: 'max-age=86400'
+    CacheControl: 'max-age=60'
   }
 
   s3.upload (uploadParams, function (err, data) {
@@ -62,49 +61,15 @@ const uploadStream = (file,s3bucket,srcPath='.') => {
   })
 }
 
-
-
-// const uploadStream = (file,s3bucket,srcPath='.') => new Promise((resolve, reject) => {
-//   const fileStream = fs.createReadStream(path.join(path.resolve(srcPath),file))
-//   fileStream.on('error', function(err) {
-//     console.log('File Error', err)
-//   })
-
-//   const uploadParams = {
-//     Bucket: s3bucket,
-//     Body: fileStream,
-//     Key: file, 
-//     ContentType: computeContentType(file),
-//     CacheControl: 'max-age=86400'
-//   }
-
-//   s3.upload (uploadParams, function (err, data) {
-//     if (err) {
-//       console.log("Error", err)
-//       reject(err)
-//     } if (data) {
-//       console.log("Upload Success", data.Location)
-//       resolve(data)
-//     }
-//   })
-// })
-
-
-
 console.log('Loading lambdaBuildBot, current dir: ' + process.cwd())
 
 exports.handler = function (event, context) {
   console.log('Received event:', JSON.stringify(event, null, 2))
   const message = event.Records[0].Sns.Message
   const mobj = JSON.parse(message)
+  // console.log(mobj)
   const archiveURL = mobj.repository.archive_url.replace('{archive_format}{/ref}','tarball/master')
   const repoDirname = localStore + '/' + mobj.repository.full_name.replace('/','-') + '-' + mobj.head_commit.id
-
-  //TODO: add optional callback to appconfig/getconfig
-  // const site = getConfig({config: repoDirname + '/config.js'})
-  // site.options.srcPath = path.join(repoDirname, site.options.srcPath) //source directory
-  // site.options.dstPath = localStore + '/dist' // destination directory for the distribution
-  // site.options.globPattern = '**/*'
 
   const reqOptions = {
     url: archiveURL,
@@ -140,46 +105,6 @@ exports.handler = function (event, context) {
   }
 
 
-  // const getBuild = () => new Promise((resolve) => {
-  //   const out = mdbuild(site)
-  //   resolve(out)
-  // })
-
-  // function getBuild (site, globPattern, callback) {
-  //   // mdbuild(site)
-  //   const out = mdbuild(site)
-  //   console.log('here:' + out)
-  //   setTimeout(() => {
-  //     callback(globPattern, {cwd: site.options.dstPath, nodir: true},out)  
-  //   }, 500)
-  // }
-
-  // function getBuild() {
-  //   return new Promise((resolve,reject) => {
-  //     const out = mdbuild(site)
-  //     console.log(out)
-  //     resolve(out)
-  //   })
-  // }
-
-
-  // const handleFiles = (files, site) => {
-  //   files.forEach(file => {
-  //     // console.log(file)
-  //     uploadStream(file, site.s3_bucket, site.options.dstPath)
-  //   })
-  // }
-
-  // const globOptions = {cwd: site.options.dstPath, nodir: true}
-
-  // const readdir = () => new Promise((resolve, reject) => {
-  //   glob(globPattern, globOptions, (err, files) => {
-  //     if (err) reject(err)
-  //     else resolve(files)
-  //   })
-  // })
-
-
   function readdir (site, callback) {
     glob(site.options.globPattern, {cwd: site.options.dstPath, nodir: true}, (err, files) => {
       if (err) console.log(err)
@@ -197,46 +122,17 @@ exports.handler = function (event, context) {
   }
 
   function uploadFiles (site, files) {
+    // TODO: add cache.replace(/[0-9]+/,300) need to change uploadParams location
     files.forEach(file => {
-      // TODO: add other endpoint options with logic for S3 vs other services
+      // TODO: add other endpoint options with logic testing for S3 vs other services
       uploadStream(file, site.s3_bucket, site.options.dstPath)
     })
   }
 
 
-  // const buildDataArray = (site, files) => new Promise((resolve, reject) => {
-  //   const promiseStack = []
-  //   // create promise array
-  //   files.forEach(file => promiseStack.push(
-  //     uploadStream(file, site.s3_bucket, site.options.dstPath)
-  //   ))
-  //   //ext html needed in dataout.obj
-  //   Promise.all(promiseStack).then(resultObj => {
-  //     resolve(resultObj)})
-  // })
-
   const build = function () {
     console.log(`'Saving: '${repoDirname}`)
     if (fs.existsSync(repoDirname)) {
-      // console.log(repoDirname + ' exists!')
-      // getBuild()
-      // readdir()
-      // .then(mylog)
-      // .catch (err => { console.log(err) })
-
-      // .then( files => { console.log(files) })
-      // .catch (err => { console.log(err) })
-
-
-      // getBuild(site)
-      // .then( () => { return readdir(globPattern, {cwd: site.options.dstPath, nodir: true}) })
-      // .then( files => { return console.log(files) })
-      // .catch (err => { console.log(err) })
-
-      // getBuild(site, globPattern, readdir)
-      // mdbuild(site,readdir)
-      // console.log(fnms)
-
       getConfig({config: repoDirname + '/config.js'}, () => {
         mdbuild(site,() => {
           readdir(site, uploadFiles)
@@ -246,11 +142,15 @@ exports.handler = function (event, context) {
       console.log(repoDirname + ' does not exist')
     }  
   }
-  
-  stream
-    .pipe(gunzip)
-    .pipe(tar.extract(localStore))
-    .on('finish', build)
 
-    // build()
+  if (mobj.ref === "refs/heads/master") {    
+    stream
+      .pipe(gunzip)
+      .pipe(tar.extract(localStore))
+      .on('finish', build)
+
+      // build() //testing
+    } else {
+      console.log(`Exiting without build/deploy...input branch was not master, instead it was ${mobj.ref}`)
+    }
 }
